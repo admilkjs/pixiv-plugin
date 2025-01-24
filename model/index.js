7f9a4b7320e20ab8949a694ec003e437b67670e1
Admilk <13205155+adrae@user.noreply.gitee.com>
2025-01-24 17:11:24 +0800

    feat(artwork): 添加作品信息解析功能，优化作品详情获取

--- model/index.js
+++ model/index.js
@@ -3,6 +3,9 @@ import {
   novelsDetail as Novel,
   seriesDetail as Novels,
 } from "./pixiv/novels.js";
-import { artworksDetail as ArtWorks } from "./pixiv/artworks.js";
+import {
+  artworksUrl as ArtWorks,
+  artworksInfo as ArtWorksInfo,
+} from "./pixiv/artworks.js";
 import { refreshPixivToken as Token } from "./utils/getCookie.js";
-export { Request, Novel, Novels, Token, ArtWorks };
+export { Request, Novel, Novels, Token, ArtWorks, ArtWorksInfo };
